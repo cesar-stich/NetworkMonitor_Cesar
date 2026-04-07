972b765bb4c08255d5469ca2d17d3002cc087dbe
cesar-stich <cesar.caqui.v@uni.pe>
2026-04-07 17:01:38 -0500

    feat: script principal uniendo modulos

---
+++ main.py
@@ -0,0 +1,12 @@
+from scanner import ping_host
+from utils import format_result
+
+# IPs de prueba (Google, la UNI, Cloudflare)
+targets = ["8.8.8.8", "9.9.9.9", "1.1.1.1"]
+
+print("--- 📡 INICIANDO MONITOREO DE RED UNI ---")
+
+for ip in targets:
+    esta_vivo = ping_host(ip)
+    mensaje = format_result(ip, esta_vivo)
+    print(mensaje)
